100f13f55652904ed14951cd434abc7044be0421
Chaewon Lee <22013437@sju.ac.kr>
2025-01-08 22:45:45 +0900

    업그레이드 UI중간 체크

--- Assets/script/Structure/UpgradeStructure.cs
+++ Assets/script/Structure/UpgradeStructure.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeStructure : StructureBase
 {
 
     [SerializeField] private GameObject existingUI; 
-    [SerializeField] private GameObject upgradeUI; 
+    [SerializeField] private GameObject upgradeUI;
+    [SerializeField] private List<Image> SrcUpgradeSlots;
+    [SerializeField] private List<Image> ElectricUpgradeSlots;
+    [SerializeField] private List<Image> HealUpgradeSlots;
 
     private bool isUpgradeUIOn; 
 
