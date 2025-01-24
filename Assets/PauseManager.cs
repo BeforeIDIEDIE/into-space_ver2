bc464a6c8a3753a76464b0fc94ca3954b227877c
Chaewon Lee <22013437@sju.ac.kr>
2025-01-25 01:30:35 +0900

    게임퍼즈를 위한 시스템 개발중

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] GameObject SRCUI;
+    [SerializeField] GameObject EventUI;
+    [SerializeField] GameObject UPGRADEUI;
+
+    void Update()
+    {
+        
+    }
+}
