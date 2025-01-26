64c5399d4e44265b0167c43d99b2dc5996294693
Chaewon Lee <22013437@sju.ac.kr>
2025-01-27 04:05:18 +0900

    이모저모

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -7,9 +7,16 @@ public class PauseManager : MonoBehaviour
     [SerializeField] GameObject SRCUI;
     [SerializeField] GameObject EventUI;
     [SerializeField] GameObject UPGRADEUI;
+    [SerializeField] GameObject PauseUI;
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(SRCUI.activeSelf)
+            {
+
+            }
+        }
     }
 }
